561c77f650a447cc7698652b63136a110353cc8b
njvision <natjel1982@gmail.com>
2023-03-15 21:58:20 +0200

    add controllers

---
+++ src/main/java/com/mdbookshop/catalogservice/web/BookController.java
@@ -0,0 +1,52 @@
+package com.mdbookshop.catalogservice.web;
+
+import com.mdbookshop.catalogservice.domain.Book;
+import com.mdbookshop.catalogservice.domain.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public Iterable<Book> getAllBooks() {
+        return bookService.viewBookList();
+    }
+
+    @GetMapping("{isbn}")
+    public Book getBookByIsbn(@PathVariable String isbn) {
+        return bookService.viewBookDetails(isbn);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBookToCatalog(book);
+    }
+
+    @DeleteMapping("{isbn}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBookByIsbn(@PathVariable String isbn) {
+        bookService.removeBookFromCatalog(isbn);
+    }
+
+    @PutMapping("{isbn}")
+    public Book updateBook(@PathVariable String isbn, @RequestBody Book book) {
+        return bookService.editBookDetails(isbn, book);
+    }
+}
